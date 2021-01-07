35cee2d6525594a75eb56c46c13dd84958118c87
Chicken <bsjohnson2004@gmail.com>
2021-01-07 17:17:44 +0000

    mAybe?

---
+++ PythonDicts.py
@@ -0,0 +1,19 @@
+
+#Shulky is definately gonna question me
+#Time to try Dict function1
+#Add to Dict
+Add=dict()
+Add['Chicken']={'They are cool'}
+print(Add)
+
+#create Dict
+Create=dict()
+
+Create=	{'Chicken': 'are gods', 'UwU': 'XD'}
+
+print(Create)
+#Check if UwU in dict, return true or false
+print('UwU' in Create)
+#Delete it then test for it
+del Create['UwU']
+print('UwU' in Create)
---
+++ loginandPasswordBasic.py
@@ -0,0 +1,7 @@
+
+import csv
+
+with open('Logins.csv','w') as R:
+    r=csv.writer(R)
+    r.writerows('Chicken')
+
